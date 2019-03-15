4ad60d44c90ff194148897157e0689b694b4cf5a
rory <rory.dudley@gmail.com>
2019-03-15 10:08:39 -0400

    adding verbose to copies in linker.sh

--- linker.sh
+++ linker.sh
@@ -10,27 +10,27 @@ printf "##############################\n"
 
 function linker() {
     # copy zsh dotfiles
-    cp -f $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
-    cp -f $HOME/dotfiles/zsh/.hushlogin $HOME/.hushlogin
-    cp -fr $HOME/dotfiles/zsh/.zsh $HOME/.zsh
+    cp -fv $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
+    cp -fv $HOME/dotfiles/zsh/.hushlogin $HOME/.hushlogin
+    cp -frv $HOME/dotfiles/zsh/.zsh $HOME/.zsh
 
     # copy screen dotfiles
-    cp -f $HOME/dotfiles/screen/.screenrc $HOME/.screenrc
+    cp -fv $HOME/dotfiles/screen/.screenrc $HOME/.screenrc
 
     # copy vim dotfiles
-    cp -f $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
-    cp -fr $HOME/dotfiles/vim/.vim $HOME/.vim
+    cp -fv $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
+    cp -frv $HOME/dotfiles/vim/.vim $HOME/.vim
 
     # copy wget dotfiles
-    cp -f $HOME/dotfiles/wget/.wgetrc $HOME/.wgetrc
+    cp -fv $HOME/dotfiles/wget/.wgetrc $HOME/.wgetrc
 
     # copy git dotfiles
-    cp -f $HOME/dotfiles/git/.gitconfig $HOME/.gitconfig
-    cp -f $HOME/dotfiles/git/.gitignore_global $HOME/.gitignore_global
-    cp -f $HOME/dotfiles/git/.stCommitMsg $HOME/.stCommitMsg
+    cp -fv $HOME/dotfiles/git/.gitconfig $HOME/.gitconfig
+    cp -fv $HOME/dotfiles/git/.gitignore_global $HOME/.gitignore_global
+    cp -fv $HOME/dotfiles/git/.stCommitMsg $HOME/.stCommitMsg
 
     # copy atom dotfiles
-    cp -fr $HOME/dotfiles/atom/.atom $HOME/.atom
+    cp -frv $HOME/dotfiles/atom/.atom $HOME/.atom
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
